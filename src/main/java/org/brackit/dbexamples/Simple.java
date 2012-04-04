e10a5e3a2f33bef3f6c4080959fb9b7ccd230786
Sebastian Baechle <Sebastian Baechle@localhost>
2012-04-04 22:28:18 +0200

    added more examples for tx management and document handling

--- src/main/java/org/brackit/dbexamples/Simple.java
+++ src/main/java/org/brackit/dbexamples/Simple.java
@@ -27,47 +27,190 @@
  */
 package org.brackit.dbexamples;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Date;
+import java.util.Random;
+
 import org.brackit.driver.BrackitConnection;
-import org.brackit.driver.BrackitException;
 
 /**
  * Basic usage scenarios.
  */
 public class Simple {
 
+	enum Severity {
+		low, high, critical
+	};
+
 	public static void main(String[] args) {
 		try {
 			singleQuery();
 			catalog();
-		} catch (BrackitException e) {
+			documentHandling();
+			txManagement();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	private static BrackitConnection getConnection() throws BrackitException {
+	private static BrackitConnection getConnection() throws Exception {
 		return new BrackitConnection("localhost", 11011);
 	}
 
-	private static void singleQuery() throws BrackitException {
+	private static void singleQuery() throws Exception {
 		BrackitConnection con = getConnection();
 		try {
-			con.query("1+2", System.out);
+			// run a simple query
+			con.query("1+1", System.out);
+			System.out.println();
 		} finally {
 			con.close();
 		}
 	}
 
-	private static void catalog() throws BrackitException {
+	private static void catalog() throws Exception {
 		BrackitConnection con = getConnection();
 		try {
 			// display the whole catalog
 			con.query("doc('_master.xml')", System.out);
-			System.out.println();
+
 			// query the catalog to get a list of all collections in the DB
+			System.out.println();
 			con.query("doc('_master.xml')//collection/@name/string()",
 					System.out);
 		} finally {
 			con.close();
 		}
+	}
+
+	private static void documentHandling() throws Exception {
+		// prepare directory with sample documents
+		File tmpDir = new File(System.getProperty("java.io.tmpdir"));
+		File dir = new File(tmpDir + File.separator + "docs"
+				+ System.currentTimeMillis());
+		if (!dir.mkdir()) {
+			throw new IOException("Directory " + dir + " already exists");
+		}
+		dir.deleteOnExit();
+		for (int i = 0; i < 10; i++) {
+			generateSampleDoc(dir, "sample", i);
+		}
+
+		BrackitConnection con = getConnection();
+		try {
+			// import documents in database
+			con.query(String.format(
+					"bit:load('mydocs.col', io:ls('%s', '\\.xml$'))", dir),
+					System.out);
+
+			// show the catalog entry of the collection
+			System.out.println();
+			con.query("doc('_master.xml')//collection[@name = '/mydocs.col']",
+					System.out);
+
+			// fetch 1. document from database
+			System.out.println();
+			con.query("fn:collection('/mydocs.col')[1]", System.out);
+
+			// create an CAS typed index for the element path '//src'
+			System.out.println();
+			con.query(
+					"bdb:create-cas-index('/mydocs.col', 'xs:string', '//src')",
+					System.out);
+
+			// show the catalog entry of the collection
+			System.out.println();
+			con.query("doc('_master.xml')//collection[@name = '/mydocs.col']",
+					System.out);
+
+			// created CAS index for the IP address range
+			// 192.168.0.0 <= ? <= 192.168.128.128
+			// (assuming that the created CAS index hast the ID 8)
+			System.out.println();
+			con.query(
+					"bdb:scan-cas-index('/mydocs.col', 8, "
+							+ "'192.168.0.0', '192.168.128.128', xs:boolean(1), xs:boolean(1), ())",
+					System.out);
+			System.out.println();
+		} finally {
+			con.close();
+		}
+	}
+
+	private static void txManagement() throws Exception {
+		BrackitConnection con = getConnection();
+		try {
+			// begin a new transaction
+			// (disable auto-commit for the following statements)
+			con.begin();
+
+			// store a sample document
+			con.query("bit:store('sample.xml', <foo><bar/></foo>)", System.out);
+
+			// commit the changes
+			con.commit();
+
+			// begin a another transaction
+			con.begin();
+
+			// display the sample document
+			con.query("doc('/sample.xml')", System.out);
+
+			// update the sample document
+			System.out.println();
+			con.query("insert node <test/> into doc('/sample.xml')/foo/bar",
+					System.out);
+
+			// display the updated sample document
+			con.query("doc('/sample.xml')", System.out);
+
+			// rollback the changes
+			con.rollback();
+
+			// display the sample document after rollback
+			System.out.println();
+			con.query("doc('/sample.xml')", System.out);
+		} finally {
+			con.close();
+		}
+	}
+
+	private static File generateSampleDoc(File dir, String prefix, int no)
+			throws IOException {
+		File file = File.createTempFile("sample", ".xml", dir);
+		file.deleteOnExit();
+		PrintStream out = new PrintStream(new FileOutputStream(file));
+		Random rnd = new Random();
+		long now = System.currentTimeMillis();
+		int diff = rnd.nextInt(6000 * 60 * 24 * 7);
+		Date tst = new Date(now - diff);
+		Severity sev = Severity.values()[rnd.nextInt(3)];
+		String src = "192.168." + (1 + rnd.nextInt(254)) + "."
+				+ (1 + rnd.nextInt(254));
+		int mlen = 10 + rnd.nextInt(70);
+		byte[] bytes = new byte[mlen];
+		int i = 0;
+		while (i < mlen) {
+			int wlen = 1 + rnd.nextInt(8);
+			int j = i;
+			while (j < Math.min(i + wlen, mlen)) {
+				bytes[j++] = (byte) ('a' + rnd.nextInt('z' - 'a' + 1));
+			}
+			i = j;
+			if (i < mlen - 1) {
+				bytes[i++] = ' ';
+			}
+		}
+		String msg = new String(bytes);
+		out.print("<?xml version='1.0'?>");
+		out.print(String.format("<log tstamp='%s' severity='%s'>", tst, sev));
+		out.print(String.format("<src>%s</src>", src));
+		out.print(String.format("<msg>%s</msg>", msg));
+		out.print("</log>");
+		out.close();
+		return file;
 	}
 }
